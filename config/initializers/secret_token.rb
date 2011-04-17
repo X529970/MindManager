719e3836b0ea9744c856afabfb1c44d1c5a0d5c5
Snuggs <rashaun.stovall@clash-media.com>
2011-04-17 17:05:45 -0400

    Initial commit containing new MindManager rails application $ rails new MindManager --skip-test-unit --skip-prototype --skip-git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MindManager::Application.config.secret_token = '15f862c4585504cbd9799d78dfd50480c7d7277cd33a0e02b71e37d880ce5723035b462144c96580fc6458f881a6537981a35039870339cad5e286f4785d5fb2'
