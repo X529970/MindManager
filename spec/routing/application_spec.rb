8b9c3d349972d02bac228aa2978fcc1602ee0c43
Snuggs <rashaun.stovall@clash-media.com>
2011-04-28 09:12:02 -0400

    Spec Thoughts routes for #new and #create

--- spec/routing/application_spec.rb
+++ spec/routing/application_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'ThoughtsController Routing' do
+describe ThoughtsController, 'Routing' do
 
   it 'has a thoughts_path' do
     { :get => thoughts_path }.should route_to :controller => 'thoughts', :action => 'index'
@@ -8,5 +8,13 @@ describe 'ThoughtsController Routing' do
 
   it 'has a redirect_path' do
     { :get => redirect_path }.should route_to :controller => 'thoughts', :action => 'redirect'
+  end
+
+  it 'has a new_path' do
+    { :get => new_thought_path }.should route_to :controller => 'thoughts', :action => 'new'
+  end
+
+  it 'has a has a create_path' do
+    { :post => create_thought_path }.should route_to :controller => 'thoughts', :action => 'create'
   end
 end
