ecfec0d69c4de35b257289977e7c7fabb7d3d145
Snuggs <rashaun.stovall@clash-media.com>
2011-04-28 11:26:02 -0400

    Formatting of Routing Specs

--- spec/routing/application_spec.rb
+++ spec/routing/application_spec.rb
@@ -3,18 +3,22 @@ require 'spec_helper'
 describe ThoughtsController, 'Routing' do
 
   it 'has a thoughts_path' do
-    { :get => thoughts_path }.should route_to :controller => 'thoughts', :action => 'index'
+    { :get => thoughts_path }.
+      should route_to :controller => 'thoughts', :action => 'index'
   end
 
   it 'has a redirect_path' do
-    { :get => redirect_path }.should route_to :controller => 'thoughts', :action => 'redirect'
+    { :get => redirect_path }.
+      should route_to :controller => 'thoughts', :action => 'redirect'
   end
 
   it 'has a new_path' do
-    { :get => new_thought_path }.should route_to :controller => 'thoughts', :action => 'new'
+    { :get => new_thought_path }.
+      should route_to :controller => 'thoughts', :action => 'new'
   end
 
   it 'has a has a create_path' do
-    { :post => create_thought_path }.should route_to :controller => 'thoughts', :action => 'create'
+    { :post => create_thought_path }.
+      should route_to :controller => 'thoughts', :action => 'create'
   end
 end
